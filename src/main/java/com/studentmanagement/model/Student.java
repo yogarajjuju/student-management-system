8e8b3a60f9268760a228403ccdee2bd1c5e2f6c0
yogarajjuju <yogarajjuju22@gmai.com>
2026-07-01 14:26:22 +0530

    Sprint 1

--- src/main/java/com/studentmanagement/model/Student.java
+++ src/main/java/com/studentmanagement/model/Student.java
@@ -1,4 +1,47 @@
 package com.studentmanagement.model;
 
 public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String department;
+
+
+    public Student(int id, String name, int age, String department){
+        this.id =id;
+        this.name=name;
+        this.age=age;
+        this.department=department;
+    }
+
+    public int setId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String  getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
