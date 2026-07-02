634f9a79147216e4306f5002d753bfda13f582a7
yogarajjuju <yogarajjuju22@gmai.com>
2026-07-02 13:49:07 +0530

    Add StudentService layer

--- src/main/java/com/studentmanagement/app/Main.java
+++ src/main/java/com/studentmanagement/app/Main.java
@@ -1,27 +1,27 @@
 package com.studentmanagement.app;
 
-import com.studentmanagement.dao.StudentDAO;
 import com.studentmanagement.model.Student;
+import com.studentmanagement.service.StudentService;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Create DAO
-        StudentDAO dao = new StudentDAO();
+        // Create Service
+        StudentService service = new StudentService();
 
         // Create Students
         Student student1 = new Student(1, "Rahul", 20, "AI&DS");
         Student student2 = new Student(2, "Priya", 21, "IT");
 
         // Add Students
-        dao.addStudent(student1);
-        dao.addStudent(student2);
+        service.addStudent(student1);
+        service.addStudent(student2);
 
         // Display All Students
         System.out.println("\n===== STUDENTS =====");
 
-        for (Student student : dao.getAllStudents()) {
+        for (Student student : service.getAllStudents()) {
             System.out.println(student);
         }
     }
--- src/main/java/com/studentmanagement/service/StudentService.java
+++ src/main/java/com/studentmanagement/service/StudentService.java
@@ -1,4 +1,20 @@
 package com.studentmanagement.service;
+import java.util.*;
+
+import com.studentmanagement.dao.StudentDAO;
+import com.studentmanagement.model.Student;
 
 public class StudentService {
+
+    private StudentDAO dao = new StudentDAO();
+
+    public void addStudent(Student student) {
+        dao.addStudent(student);
+
+    }
+    public List<Student> getAllStudents() {
+
+        return dao.getAllStudents();
+
+    }
 }
