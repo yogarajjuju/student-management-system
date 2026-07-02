517c4063a4934f6f47b3eec4e300313544b6ff6b
yogarajjuju <yogarajjuju22@gmai.com>
2026-07-02 13:34:32 +0530

    Implement in-memory StudentDAO

--- src/main/java/com/studentmanagement/app/Main.java
+++ src/main/java/com/studentmanagement/app/Main.java
@@ -1,4 +1,28 @@
 package com.studentmanagement.app;
 
+import com.studentmanagement.dao.StudentDAO;
+import com.studentmanagement.model.Student;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        // Create DAO
+        StudentDAO dao = new StudentDAO();
+
+        // Create Students
+        Student student1 = new Student(1, "Rahul", 20, "AI&DS");
+        Student student2 = new Student(2, "Priya", 21, "IT");
+
+        // Add Students
+        dao.addStudent(student1);
+        dao.addStudent(student2);
+
+        // Display All Students
+        System.out.println("\n===== STUDENTS =====");
+
+        for (Student student : dao.getAllStudents()) {
+            System.out.println(student);
+        }
+    }
 }
--- src/main/java/com/studentmanagement/dao/StudentDAO.java
+++ src/main/java/com/studentmanagement/dao/StudentDAO.java
@@ -1,4 +1,27 @@
 package com.studentmanagement.dao;
 
+// imports
+import java.util.ArrayList;
+import java.util.List;
+
+import com.studentmanagement.model.Student;
+
 public class StudentDAO {
+
+    // Store students here
+    private ArrayList<Student> students =new ArrayList<>();
+
+
+    public void addStudent(Student student){
+        students.add(student);
+        System.out.println("Student added successfully!");
+
+    }
+    public List<Student> getAllStudents() {
+
+        // ???
+        return students;
+
+    }
+
 }
